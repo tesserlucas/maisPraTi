ef3d897bde7c2610e284c3ddff31559ee2e28802
Lucas Tesser <lucastesser39@hotmail.com>
2024-06-24 17:09:00 -0300

    criação de um landing page

---
+++ landing_page/js/script.js
@@ -0,0 +1,9 @@
+document.getElementById('btn-projetos').addEventListener('click', function() {
+    document.getElementById('projetos').scrollIntoView();
+});
+document.getElementById('btn-sobre').addEventListener('click', function() {
+    document.getElementById('sobre').scrollIntoView();
+});
+document.getElementById('btn-contato').addEventListener('click', function() {
+    document.getElementById('contato').scrollIntoView();
+});
